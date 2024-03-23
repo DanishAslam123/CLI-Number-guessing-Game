c92ff0221a67b478d59a5f3414987f15bdbb200d
Danish <157038703+DanishAslam123@users.noreply.github.com>
2024-03-24 03:37:55 +0500

    My First Comment

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CLI-Number-guessing-Game" 
---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let randomnumber = Math.floor(Math.random() * 3);
+console.log("\n\n\tWelcome To Number Guessing Game.\n\n \tGuess the Number Between 1 to 3.\n\n\t\t'CONDITION'\n\n\tYour First Number or Second Number are Same enter then your are win ! \n\n");
+const answer = await inquirer.prompt([{
+        message: "Enter your Guess Number !",
+        type: "number",
+        name: "Your_Guess_Number",
+    },
+    { message: "Enter your Second Guess Number",
+        type: "number",
+        name: "Your_Second_Guess_number" },
+]);
+if (answer.Your_Guess_Number === randomnumber) {
+    console.log("'Congratulation'! Your Guessed Number is Match");
+}
+if (answer.Your_Second_Guess_number = answer.Your_Guess_Number === randomnumber) {
+    console.log("");
+}
+else {
+    console.log("Your Guessed Number is Not Match Please Try Again !");
+}
